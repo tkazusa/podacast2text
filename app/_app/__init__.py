4b9c4505061c40d9a0e30dc5cf5f34a3ad7da927
tkazusa <takekazusa@gmail.com>
2019-01-26 16:58:40 +0000

    mod dir structure

--- app/_app/__init__.py
+++ app/_app/__init__.py
@@ -1,56 +1,15 @@
 # -*- encoding: UTF-8 -*-
-import os
-from flask import Flask, flash, request, redirect, url_for, send_from_directory
-from werkzeug.utils import secure_filename
+from flask import Flask
 from google.cloud import storage
 
-UPLOAD_FOLDER = '/path/to/the/uploads'
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
-
-app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-
-def allowed_file(filename: str) -> str:
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+from .views import transcriber
 
 
 def create_app() -> type(Flask):
     app = Flask(__name__)
     app.config['JSON_AS_ASCII'] = False
+    app.register_blueprint(transcriber)
+
     client = storage.Client()
 
-    @app.route('/', methods=['GET', 'POST'])
-    def upload_file() -> str:
-        if request.method == 'POST':
-            # check if the post request has the file part
-            if 'file' not in request.files:
-                flash('No file part')
-                return redirect(request.url)
-            file = request.files['file']
-            # if user does not select file, browser also
-            # submit an empty part without filename
-            if file.filename == '':
-                flash('No selected file')
-                return redirect(request.url)
-            if file and allowed_file(file.filename):
-                filename = secure_filename(file.filename)
-                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-                return redirect(url_for('uploaded_file',
-                                        filename=filename))
-        return '''
-        <!doctype html>
-        <title>Upload new File</title>
-        <h1>Upload new File</h1>
-        <form method=post enctype=multipart/form-data>
-          <input type=file name=file>
-          <input type=submit value=Upload>
-        </form>
-        '''
- 
-    @app.route('/uploads/<filename>')
-    def uploaded_file(filename) -> str:
-        return send_from_directory(app.config['UPLOAD_FOLDER'],
-                                   filename)
     return app
---
+++ app/_app/views/__init__.py
@@ -0,0 +1,4 @@
+# -*- encoding: UTF-8 -*-
+from flask import Blueprint
+
+transcriber = Blueprint('transcriber', __name__)
