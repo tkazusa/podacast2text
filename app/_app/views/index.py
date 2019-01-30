12e5d6ba0c3d81e7a4ca4f55a6febd1adf3058f8
tkazusa <takekazusa@gmail.com>
2019-01-30 08:09:22 +0000

    mod dir structure with blueprint

--- app/_app/views/index.py
+++ app/_app/views/index.py
@@ -5,11 +5,8 @@ from werkzeug.utils import secure_filename
 
 from . import transcriber
 
-UPLOAD_FOLDER = '/path/to/the/uploads'
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
-app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 
 def allowed_file(filename: str) -> str:
