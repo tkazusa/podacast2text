64ccc65cc10a949e15255b57d2342980464637f8
tkazusa <takekazusa@gmail.com>
2019-03-28 13:45:03 +0000

    add transcribe.html

--- app/_app/views/index.py
+++ app/_app/views/index.py
@@ -1,73 +1,52 @@
 # -*- coding: UTF-8 -*-
-import os
-
-from flask import (Flask, flash, redirect, render_template, request,
-                   send_from_directory, url_for)
+from flask import render_template, request
 from google.cloud import storage
-
-from werkzeug.utils import secure_filename
 
 from . import transcriber
 
+bucket_name = 'bp_yead'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 
 def allowed_file(filename: str) -> str:
+    """The allowed file names
+    Args:
+        filename (str): filename
+    """
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-bucket_name = 'bp_yead'
-
-
-def upload_blob(bucket_name, uploaded_file, destination_blob_name):
-    """Uploads a file to the bucket."""
+def upload_blob(bucket_name, uploaded_file, blob_filename):
+    """Uploads a file to the bucket.
+    Args:
+        bucket_name (str): The bucket path on GCS
+        uploaded_file : file object from post request
+        blob_filename (str): filename of the destination blob
+    """
     storage_client = storage.Client()
     bucket = storage_client.get_bucket(bucket_name)
-    blob = bucket.blob(destination_blob_name)
+    blob = bucket.blob(blob_filename)
 
     blob.upload_from_file(uploaded_file)
 
 
 @transcriber.route('/', methods=['GET'])
 def index() -> str:
-    """
-    # check if the post request has the file part
-    if 'file' not in request.files:
-        flash('No file part')
-        return redirect(request.url)
-    file = request.files['file']
-    # if user does not select file, browser also
-    # submit an empty part without filename
-    if file.filename == '':
-        flash('No selected file')
-        return redirect(request.url)
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        return redirect(url_for('uploaded_file',
-                                filename=filename))
-    """
     return render_template('index.html', title='index')
 
 
 @transcriber.route('/upload', methods=['POST'])
-def upload():
+def upload() -> str:
     uploaded_file = request.files.get('file')
 
     if 'file' not in request.files:
         return 'No file uploaded.', 400
 
-    destination_blob_name = uploaded_file.filename
-    upload_blob(bucket_name, uploaded_file, destination_blob_name)
+    blob_filename = uploaded_file.filename
+    upload_blob(bucket_name, uploaded_file, blob_filename)
 
     msg = 'File {} uploaded to {}.'.format(
-        uploaded_file, destination_blob_name)
+        uploaded_file, blob_filename)
 
-    return msg
-
-
-@transcriber.route('/uploads/<filename>')
-def uploaded_file(filename) -> str:
-    return send_from_directory(app.config['UPLOAD_FOLDER'],
-                               filename)
+    return render_template('transcribe.html', message=msg)
